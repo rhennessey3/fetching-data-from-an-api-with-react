30710b58c1d2d5fc94cfb46bf50bed46566dd6d5
Richard Hennessey III <Rick@Richards-iMac.local>
2020-06-07 14:23:52 -0400

    added loading screen functionality

--- src/App.js
+++ src/App.js
@@ -8,24 +8,28 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
+      loading: false,
       character: {}
     }
   }
 
   componentDidMount() {
+    this.setState({loading: true})
     fetch("https://swapi.dev/api/people/1")
       .then(response => response.json())
       .then(data => {
         this.setState({
+          loading: false,
           character: data
         })
       })
   }
 
   render() {
+    const text = this.state.loading ? "loading.." : this.state.character.name
     return (
       <div>
-        {this.state.character.name}
+        {text}
       </div>
     )
   }
